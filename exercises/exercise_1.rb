6c55270279d26275a6cc6b8961f43f8702f6ade0
Matt Rostad <matt@rostad.ca>
2023-09-04 16:10:16 -0700

    ex 1 & 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,15 @@
 require_relative '../setup'
+require 'active_record'
 
-puts "Exercise 1"
-puts "----------"
+# puts "Exercise 1"
+# puts "----------"
 
-# Your code goes below here ...
+class Store < ActiveRecord::Base
+end
+
+store1 = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store3 = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# Output the number of stores in the database
+puts "Number of stores: #{Store.count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,30 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+require 'active_record'
+
+# Define the Store class
+class Store < ActiveRecord::Base
+end
+
+# Load the first store with id = 1
+@store1 = Store.find(1)
+
+# Load the second store
+@store2 = Store.find(2)
+
+# Update the first store's name
+@store1.update(name: 'Matts store')
+
+# You can also update other attributes if needed
+# For example, to change the annual_revenue:
+# store1.update(annual_revenue: new_annual_revenue)
+
+# Check if the update was successful
+if @store1.valid?
+  puts "Store 1 updated successfully."
+else
+  puts "Error updating Store 1."
+end
+
+
